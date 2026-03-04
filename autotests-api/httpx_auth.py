909a4f7ca457ff62e23028cd8071a39196f09332
lavrusvadik15-star <lavrus.vadik15@gmail.com>
2026-03-04 15:01:43 +0300

    Задание

--- autotests-api/httpx_auth.py
+++ autotests-api/httpx_auth.py
@@ -12,13 +12,26 @@ print(f"Login response: {login_response_data}")
 print(f"Status code: {login_response.status_code}")
 
 
-refresh_payload = {
-  "refreshToken": login_response_data['token']['refreshToken']
+# refresh_payload = {
+#   "refreshToken": login_response_data['token']['refreshToken']
+# }
+#
+#
+# refresh_response = httpx.post("http://localhost:8000/api/v1/authentication/refresh", json = refresh_payload)
+# refresh_response_data = refresh_response.json()
+#
+# print(f"Refresh response: {refresh_response_data}")
+# print(f"Status code: {refresh_response.status_code}")
+
+me_header = {
+        "Authorization" : f"Bearer {login_response_data['token']['accessToken']}"
 }
 
+print(me_header)
 
-refresh_response = httpx.post("http://localhost:8000/api/v1/authentication/refresh", json = refresh_payload)
-refresh_response_data = refresh_response.json()
 
-print(f"Refresh response: {refresh_response_data}")
-print(f"Status code: {refresh_response.status_code}")
+user_me = httpx.get("http://localhost:8000/api/v1/users/me", headers = me_header)
+me_data = user_me.json()
+
+print(user_me.status_code)
+print(me_data)
--- autotests-api/httpx_example.py
+++ autotests-api/httpx_example.py
@@ -1,61 +1,61 @@
 import httpx
 
-# response = httpx.get('https://jsonplaceholder.typicode.com/todos/1')
-# print(response.status_code)
-# print(response.json())
-#
-# data = {
-#     "title" : "ТЕСТ",
-#     "completed" : False,
-#     "userId" : 1
-#
-# }
-# response = httpx.post("https://jsonplaceholder.typicode.com/todos", json=data)
-#
-# print(response.status_code)
-# print(response.json())
-# #print(response.request.headers)
-#
-# dara = {
-#     "username": "name",
-#     "password": "12345"
-# }
-# response = httpx.post("https://httpbin.org/post", data = data)
-# print(response.status_code)
-# print(response.json())
-# #print(response.request.headers)
-#
-# headers = {
-#     "Autor" : "Secret"
-# }
-# response = httpx.get("https://httpbin.org/get", headers = headers)
-# print(response.status_code)
-# print(response.json())
-#
-# response = httpx.get("https://jsonplaceholder.typicode.com/todos?userId=1")
-# print(response.url)
-# print(response.json())
-#
-# params = {
-#     "userID" : 1
-# }
-# response = httpx.get("https://jsonplaceholder.typicode.com/todos", params=params)
-# print(response.url)
-# print(response.json())
-#
-# files = {"file" : ("example.txt", open("example.txt", "rb"))}
-# response = httpx.post("https://httpbin.org/post", files = files)
-# print(response.json())
-#
-# with httpx.Client() as client :
-#     response1= client.get ("https://jsonplaceholder.typicode.com/todos/1")
-#     response2= client.get("https://jsonplaceholder.typicode.com/todos/1")
-# print(response1.json())
-# print(response2.json())
-#
-# client = httpx.Client(headers = {"Autor" : "Secret"})
-# response = client.get("https://httpbin.org/get")
-# print(response.json())
+response = httpx.get('https://jsonplaceholder.typicode.com/todos/1')
+print(response.status_code)
+print(response.json())
+
+data = {
+    "title" : "ТЕСТ",
+    "completed" : False,
+    "userId" : 1
+
+}
+response = httpx.post("https://jsonplaceholder.typicode.com/todos", json=data)
+
+print(response.status_code)
+print(response.json())
+#print(response.request.headers)
+
+data = {
+    "username": "name",
+    "password": "12345"
+}
+response = httpx.post("https://httpbin.org/post", data = data)
+print(response.status_code)
+print(response.json())
+#print(response.request.headers)
+
+headers = {
+    "Autor" : "Secret"
+}
+response = httpx.get("https://httpbin.org/get", headers = headers)
+print(response.status_code)
+print(response.json())
+
+response = httpx.get("https://jsonplaceholder.typicode.com/todos?userId=1")
+print(response.url)
+print(response.json())
+
+params = {
+    "userID" : 1
+}
+response = httpx.get("https://jsonplaceholder.typicode.com/todos", params=params)
+print(response.url)
+print(response.json())
+
+files = {"file" : ("example.txt", open("example.txt", "rb"))}
+response = httpx.post("https://httpbin.org/post", files = files)
+print(response.json())
+
+with httpx.Client() as client :
+    response1= client.get ("https://jsonplaceholder.typicode.com/todos/1")
+    response2= client.get("https://jsonplaceholder.typicode.com/todos/1")
+print(response1.json())
+print(response2.json())
+
+client = httpx.Client(headers = {"Autor" : "Secret"})
+response = client.get("https://httpbin.org/get")
+print(response.json())
 try :
     response = httpx.get("https://jsonplaceholder.typicode.com/invalidTEST")
     #print(response.status_code)
